971a6e7166fdbe6c6685453fb8278325bb7b6e8f
Enio L. Sombra <eniosombra@gmail.com>
2021-05-20 17:53:17 -0300

    challenge finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+import { GenreResponseProps } from '../App';
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClick: (id: number) => void;
+};
+
+export function SideBar({
+  genres,
+  handleClick,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
